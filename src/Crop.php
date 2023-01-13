5ce69ad11d5c9015c67b138ba81d3ed92dd41f85
igwen6w <igwen6w@gmail.com>
2023-01-13 22:13:17 +0800

    2.1 use CropTrait

--- src/Crop.php
+++ src/Crop.php
@@ -5,7 +5,9 @@ namespace Igwen6w\Cropper;
 use Encore\Admin\Form\Field\Image;
 
 class Crop extends Image
-{   
+{
+    use CropTrait;
+
     private $aspect_ration = 1.777778;
 
     protected static $css = [
@@ -16,23 +18,5 @@ class Crop extends Image
         '/vendor/igwen6w/cropper/cropperjs-1.5.13/dist/cropper.min.js',
         '/vendor/igwen6w/cropper/main.js',
     ];
-
-    public function aspectRation($aspect_ration)
-    {
-        if (!empty($aspect_ration) && is_numeric($aspect_ration) && $aspect_ration > 0) {
-            $this->attributes['data-aspect-ration'] = $aspect_ration;
-        } else {
-            $this->attributes['data-aspect-ration'] = $this->aspect_ration;
-        }
-        return $this;
-    }
-
-    protected function setupScripts($options)
-    {
-        parent::setupScripts($options);
-        $this->script .= <<<SCRIPT
-let el = $("input{$this->getElementClassSelector()}");runCropper(el);
-SCRIPT;
-    }
 
 }
