8ca56ddc54b35b1970cc8adf5731734261c70662
igwen6w <igwen6w@gmail.com>
2023-01-13 22:12:35 +0800

    2.1

---
+++ src/CropTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Igwen6w\Cropper;
+
+trait CropTrait
+{
+    public function aspectRation($aspect_ration)
+    {
+        if (!empty($aspect_ration) && is_numeric($aspect_ration) && $aspect_ration > 0) {
+            $this->attributes['data-aspect-ration'] = $aspect_ration;
+        } else {
+            $this->attributes['data-aspect-ration'] = $this->aspect_ration;
+        }
+        return $this;
+    }
+
+    protected function setupScripts($options)
+    {
+        parent::setupScripts($options);
+        $this->script .= <<<SCRIPT
+runCropper($("input{$this->getElementClassSelector()}"));
+SCRIPT;
+    }
+
+}
