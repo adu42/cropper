7d651d574826f35f808b12d55abac4276ba8cc47
ado <adu42@users.noreply.github.com>
2025-05-31 18:38:59 +0800

    Update Crop.php

--- src/Crop.php
+++ src/Crop.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Encore\Cropper;
+namespace Adu42\Cropper;
 
 use Encore\Admin\Form\Field\Image;
 
@@ -11,13 +11,13 @@ class Crop extends Image
     private $aspect_ration = 1.777778;
 
     protected static $css = [
-        '/vendor/encore/cropper/cropperjs-1.5.13/dist/cropper.min.css',
+        '/vendor/adu42/cropper/cropperjs-1.5.13/dist/cropper.min.css',
     ];
 
     protected static $js = [
-        '/vendor/encore/bootstrap-fileinput/js/fileinput.min.js',
-        '/vendor/encore/cropper/cropperjs-1.5.13/dist/cropper.min.js',
-        '/vendor/encore/cropper/main.js',
+        '/vendor/adu42/bootstrap-fileinput/js/fileinput.min.js',
+        '/vendor/adu42/cropper/cropperjs-1.5.13/dist/cropper.min.js',
+        '/vendor/adu42/cropper/main.js',
     ];
 
 }
